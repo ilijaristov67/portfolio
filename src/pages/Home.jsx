826d9efce960d147a403cd601f280a9c3a965c63
ilijaristov67 <ilijaristov67@gmail.com>
2026-02-26 12:05:03 +0100

    navbar done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,20 @@
 import { ThemeToggle } from "@/components/ThemeToggle.jsx";
 import { StarBackground } from "@/components/StarBackground.jsx";
+import {Navbar} from "@/components/Navbar.jsx";
 
 export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
       {/* Theme Toggle */}
-      <ThemeToggle />
+        <ThemeToggle />
       {/* Background Effects */}
         <StarBackground/>
       {/* Navbar  */}
-
+        <Navbar/>
       {/* Main Content */}
-
+        <main>
+            <HeroSection/>
+        </main>
       {/* Footer */}
     </div>
   );
