6691cfbc9b942b19f9852c508819596aa0d4001e
Ilija <ilijaristov67@gmail.com>
2026-03-01 12:36:30 +0100

    almost done with projects

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -53,6 +53,13 @@ export const ProjectsSection = () => {
                             ))}
                         </div>
                       </div>
+
+                      <h3 className="text-xl font-semibold mb-1">
+                          {project.title}
+                      </h3>
+                      <p className="text-muted-foreground text-sm mb-4">
+                          {project.description}
+                      </p>
                   </div>
               ))}
           </div>
